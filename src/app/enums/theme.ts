81d8439d62fcb905f7a0f58693a8a669e73f43a7
mirzagholi <m.mirzagholi@megagroup.biz>
2023-09-14 22:09:01 +0330

    delete all files

--- src/app/enums/theme.ts
+++
@@ -1,4 +0,0 @@
-export enum ThemeType {
-    light = `theme-light`,
-    dark = `theme-dark`
-}
--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import { ThemeType } from "../enums/theme";
-
-export const environment = {
-  production: true,
-  theme: ThemeType.light,
-};
